72b52ae6b32d8f81c06b1f06bf8cfb9dfad6e7d0
Maurice Huang <maurice.huang@goalsmapper.com>
2023-08-21 14:25:22 +0800

    check for valid data.session and redirect to main page and protect route

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,7 +1,14 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
-import { children } from "react";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function AuthLayout({ children }) {
+export default async function AuthLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+  if (data.session) {
+    redirect("/");
+  }
   return (
     <>
       <nav>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,13 +1,25 @@
-import Image from "next/image";
 import Link from "next/link";
-import logo from "./dojo-logo.png";
-export default function Navbar() {
+import Image from "next/image";
+import Logo from "./dojo-logo.png";
+import LogoutButton from "./LogoutButton";
+
+export default function Navbar({ user }) {
   return (
     <nav>
-      <Image src={logo} width={70} placeholder="blur" alt="a company logo" />
-      <h1>Maurice Helpdesk</h1>
-      <Link href="/">Home</Link>
-      <Link href="/tickets">Tickets</Link>
+      <Image
+        src={Logo}
+        alt="Dojo Helpdesk logo"
+        width={70}
+        placeholder="blur"
+        quality={100}
+      />
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets" className="mr-auto">
+        Tickets
+      </Link>
+      {user && <span>Hello, {user.email}</span>}
+      <LogoutButton />
     </nav>
   );
 }
